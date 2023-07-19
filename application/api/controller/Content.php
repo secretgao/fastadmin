650729446c7829ced5e30dea8ffd23f5795ed5ea
secretgao <891841626@qq.com>
2023-07-19 14:34:23 +0800

    Update Content.php

--- application/api/controller/Content.php
+++ application/api/controller/Content.php
@@ -148,7 +148,11 @@ class Content extends Api
         }
         $models = $model->where(['status' => 'normal', 'id' => $id]);
         $data = $models->field('id,cateid,title,litetitle,avatar,detail,mes_avatar,mes')->find();
-
+        if ($data){
+            $host = $_SERVER['HTTP_HOST'];
+            $data = $data->toArray();
+            $data['mes_avatar'] = 'http://' . $host . $data['mes_avatar'];
+        }    
 
         $this->success('返回成功', $data ? $data->toArray() : []);
     }
